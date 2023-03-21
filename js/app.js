91f9a5541effc99b067397344745e2250eda9113
Masoumeh Rezai <you@example.com>
2023-03-21 09:28:25 +0430

    responsive section

--- js/app.js
+++ js/app.js
@@ -2,18 +2,12 @@ function data() {
     let name = window.document.getElementsByClassName("post");
     for (let x = 0; x < name.length; x++) {
         if (name[x].value == "") {
-            name[x].style.backgroundColor = "pink";
+            name[x].style.backgroundColor = "purple";
         }
         else {
             name[x].style.backgroundColor = "white"
 
         }
     }
-
-
-
-
-
-
 }
 
